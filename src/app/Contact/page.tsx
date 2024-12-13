5b27dceabc41c6947b0441597b1cac5f0913c29e
Hillary <hillary.ferreir@gmail.com>
2024-12-13 18:21:40 -0300

    feat: pagina contato

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,10 +1,33 @@
-// import styles from './Contact.module.css'
+import styles from './Contact.module.css'
+import { MdEmail } from 'react-icons/md'
+import { IoLogoWhatsapp } from 'react-icons/io'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+
 
 export default function Contact(){
 
     return(
-        <>
-        <h2>Entre em contato comigo</h2>
-        </>
+        <section id={styles.container}>
+                <h1>Entre em contato comigo</h1>
+                <p>Assim podemos conversar mais</p>
+                <div className={styles.icons}>
+                    <div className={styles.icon}>
+                        <MdEmail className={styles.ico}/>
+                        <p>hillary.developer@gmail.com</p>
+                    </div>
+                    <div className={styles.icon}>
+                        <IoLogoWhatsapp className={styles.ico}/>
+                        <p>(11) 93339-0122</p>
+                    </div>
+                    <div className={styles.icon}>
+                        <FaLinkedin className={styles.ico}/>
+                        <p>Hillary Santos Ferreira</p>
+                    </div>
+                    <div className={styles.icon}>
+                        <FaGithub className={styles.ico}/>
+                        <p>Hillary33</p>
+                    </div>
+                </div>
+        </section>
     )
 }
