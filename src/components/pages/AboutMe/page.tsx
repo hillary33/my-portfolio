11b869021ef9bf3c60a86ab04b850f92e14939b2
hillary33 <hillary.ferreir@gmail.com>
2025-04-06 09:43:51 -0300

    fix: correção about me

--- src/components/pages/AboutMe/page.tsx
+++ src/components/pages/AboutMe/page.tsx
@@ -8,7 +8,7 @@ export default function AboutMe(){
             <section className={styles.container}> 
                 <h1>Sobre mim</h1>
                 <p>
-                Oii, tenho 20 anos e minha jornada no mundo da programação começou em 2021, quando conheci o básico de html por curiosidade. Em 2022, fui aprovada no curso de Análise e Desenvolvimento de Sistemas (ADS) com o ENEM, e desde então, tenho me dedicado cada vez mais a essa área que me fascina. Ao longo do meu percurso acadêmico, tive a oportunidade de participar e colaborar em diversos projetos, que não só ampliaram meu conhecimento técnico, mas também fortaleceram minha vontade de aprender e me aprimorar constantemente. A cada desafio, minha paixão por desenvolver soluções criativas e inovadoras só cresce, e estou cada vez mais motivada a me aprofundar no universo do desenvolvimento web.
+                Oii, tenho 20 anos e minha jornada no mundo da programação começou em 2021, quando conheci o básico de html por curiosidade. Em 2022, fui aprovada no curso de Análise e Desenvolvimento de Sistemas (ADS) pelo o ENEM, e desde então, tenho me dedicado cada vez mais a essa área que me fascina. Ao longo do meu percurso acadêmico, tive a oportunidade de participar e colaborar em diversos projetos, que não só ampliaram meu conhecimento técnico, mas também fortaleceram minha vontade de aprender e me aprimorar constantemente. A cada desafio, minha paixão por desenvolver soluções criativas e inovadoras só cresce, e estou cada vez mais motivada a me aprofundar no universo do desenvolvimento web.
                 </p>
                 <h2>Habilidades</h2>
                 <div className={styles.cards}>
